1e875a0b742829f1e86131f3f118940634483dbf
Thanh Tran <tranthanhdeveloper@gmail.com>
2023-01-02 21:31:14 +0700

    Initial commit

---
+++ src/test/java/com/programmingsharing/kafkaspringboot/KafkaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programmingsharing.kafkaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
